bc189fd433b257d561aa0a38efd7a5fa642ef424
Mail Manager Dev <anthropic@technomonkey.net>
2026-04-21 20:29:12 -0700

    chore(27-02): export sentinel IMAP operations from barrel
    - Re-export appendSentinel, findSentinel, deleteSentinel, runSentinelSelfTest
    - Re-export AppendSentinelResult type

--- src/sentinel/index.ts
+++ src/sentinel/index.ts
@@ -2,3 +2,5 @@ export { buildSentinelMessage, purposeBody } from './format.js';
 export type { SentinelMessage, BuildSentinelOpts, FolderPurpose } from './format.js';
 export { SentinelStore } from './store.js';
 export type { Sentinel, SentinelRow } from './store.js';
+export { appendSentinel, findSentinel, deleteSentinel, runSentinelSelfTest } from './imap-ops.js';
+export type { AppendSentinelResult } from './imap-ops.js';
